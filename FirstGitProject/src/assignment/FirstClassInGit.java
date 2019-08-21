3e199587b5590972fc0df97147e9640c152f285d
ms13797 <ms13797@CEIT2553214X026.ad.georgiasouthern.edu>
2019-08-21 16:50:52 -0400

    ● Added input for last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,21 @@
 package assignment;
+import java.util.Scanner;
+
 
 public class FirstClassInGit {
 
+	private static Scanner input;
+
 	public static void main(String[] args) {
 		System.out.println("Hello, my name is Mureed!");
+		
+		input = new Scanner(System.in);
+		
+		System.out.print("What is your last name? ");
+		String lastName = input.next();
+		
+		System.out.println("Nice to meet you, " + lastName + "!");
+		
 		
 		//add a statement to display your name in the console
 		
